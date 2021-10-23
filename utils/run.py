1e8db6c997bf067e8477fb9c852e8e95620f0808
AndrewFeng <fze17@mails.tsinghua.edu.cn>
2021-10-23 22:08:23 +0800

    add changes

--- utils/run.py
+++ utils/run.py
@@ -1,8 +1,9 @@
 import sys
 import os
+import logging
+
 import torch
-
-
+import transformers
 from transformers import (
     HfArgumentParser,
     Trainer,
@@ -11,10 +12,16 @@ from transformers import (
     AutoModelForMaskedLM,
     TrainingArguments,
 )
+from transformers.trainer_utils import is_main_process
 
 from arguments import DataTrainingArguments, ModelArguments
 
+logger = logging.getLogger(__name__)
+
 def main():
+    '''
+        Parsing arguments
+    '''
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
@@ -22,7 +29,47 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    print(model_args, data_args, training_args)
+    
+    '''
+        Make directories for output
+    '''
+    if (
+        os.path.exists(training_args.output_dir)
+        and os.listdir(training_args.output_dir)
+        and training_args.do_train
+        and not training_args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+            "Use --overwrite_output_dir to overcome."
+        )
+
+    '''
+        Set Logging
+    '''
+    logging.setbasicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if is_main_process(training_args.local_rank) else logging.WARN,
+    )
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+
+    # Set the verbosity to info of the Transformers logger (on main process only):
+    if is_main_process(training_args.local_rank):
+        transformers.utils.logging.set_verbosity_info()
+        transformers.utils.logging.enable_default_handler()
+        transformers.utils.logging.enable_explicit_format()
+    logger.info(f"Training/evaluation parameters {training_args}")
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+    if data_args.train_file.endswith(".csv"):
+        # Loading a dataset from local csv files
+        datasets = load_dataset(
+            "csv", data_files={"train": data_args.train_file, "validation": data_args.validation_file, "test": data_args.test_file}
+        )
 
 
 if __name__ == '__main__':
